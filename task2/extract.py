70e069c8c344d75907ba2ed4906ba57bcb202e1f
Lucas <lucaswelch7@gmail.com>
2023-05-28 22:55:19 -0700

    V2

--- task2/extract.py
+++ task2/extract.py
@@ -1,23 +1,24 @@
 import json
 import re
 
-def extract_q_and_a(text): 
+def extract_q_and_a(text):
     question_answer_pattern = r'Q:\s*(.*?)\s*\nA:\s*(.*)'
-# Executes a regex pattern to find questions  and answers in text
-
-    matches = re.findall(question_answer_pattern, text) 
+# Regex patterns to find questions and answers
+    matches = re.findall(question_answer_pattern, text)
     questions = [match[0] for match in matches]
     answers = [match[1] for match in matches]
     return questions, answers
-# Return all non-overlapping matches of pattern in string, as a list of strings or tuples
 
-def create_qa_json(questions, answers): 
-    qa_pairs = [{"questions": q, "answer": a} for q, a in zip(questions, answers)]
-    with open ('qa_pairs.json', 'w') as f: 
-        json.dump(qa_pairs, f)
+def create_qa_json(questions, answers):
+    qa_pairs = [{"question": q, "answer": a} for q, a in zip(questions, answers)]
+# Creates a dictionary with key "questions" and value as the list of qa_pairs
+    dict_to_dump = {"questions": qa_pairs}
+    with open ('qa_pairs.json', 'w') as f:
+        json.dump(dict_to_dump, f) 
+# Dumps the newly created dictionary instead of qa_pairs
 
 def main():
-    with open('textbook.txt', 'r') as f: 
+    with open('textbook.txt', 'r') as f:
         text = f.read()
 
     questions, answers = extract_q_and_a(text)
--- task2/extract_test.py
+++ task2/extract_test.py
@@ -1,31 +1,26 @@
 import unittest
+from extract import extract_q_and_a, create_qa_json
 import json
 import os
-from extract import extract_q_and_a, create_qa_json
 
-class TestExtract(unittest.TestCase):
-
+class TestQAFunctions(unittest.TestCase):
     def test_extract_q_and_a(self):
-        text = "Q: What is Python? \nA: Python is a programming language."
+        text = 'Q: What is Python?\nA: Python is a programming language.'
         questions, answers = extract_q_and_a(text)
-
-        self.assertEqual(questions, ["What is Python?"])
-        self.assertEqual(answers, ["Python is a programming language."])
-
-class TestCreateJSON(unittest.TestCase):
+        self.assertEqual(questions, ['What is Python?'])
+        self.assertEqual(answers, ['Python is a programming language.'])
 
     def test_create_qa_json(self):
-        questions = ["What is Python?"]
-        answers = ["Python is a programming language."]
+        questions = ['What is Python?']
+        answers = ['Python is a programming language.']
         create_qa_json(questions, answers)
-        
-        # Open the file again and check if the content is correct
+
         with open('qa_pairs.json', 'r') as f:
             data = json.load(f)
+        self.assertEqual(data['questions'][0]['question'], 'What is Python?')
+        self.assertEqual(data['questions'][0]['answer'], 'Python is a programming language.')
 
-        self.assertEqual(data, [{"questions": "What is Python?", "answer": "Python is a programming language."}])
-        
-        # Clean up after the test by removing the file
+        # Remove the file after testing
         os.remove('qa_pairs.json')
 
 if __name__ == '__main__':
